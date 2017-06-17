13cf53625d1b99023afe46c0cfb3830a373a70a3
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-06-17 16:21:15 +0530

    Changed Win32 to ${Arch}

--- cmake/scripts/windows/ArchSetup.cmake
+++ cmake/scripts/windows/ArchSetup.cmake
@@ -24,11 +24,7 @@ list(APPEND CMAKE_SYSTEM_LIBRARY_PATH ${CMAKE_SOURCE_DIR}/project/BuildDependenc
 list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_SOURCE_DIR}/project/BuildDependencies/${ARCH})
 list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_SOURCE_DIR}/project/BuildDependencies)
 
-if(${ARCH} STREQUAL win32)
-  set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/Win32/include/python)
-else()
-  set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/${ARCH}/include/python)
-endif()
+set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/${ARCH}/include/python3.6)
 
 
 # -------- Compiler options ---------
