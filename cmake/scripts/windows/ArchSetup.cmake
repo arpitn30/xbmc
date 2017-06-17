0723ae7c078909df365db8ec4556cfbada984367
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-06-17 14:32:03 +0530

    Initial Commit

--- cmake/scripts/windows/ArchSetup.cmake
+++ cmake/scripts/windows/ArchSetup.cmake
@@ -25,7 +25,7 @@ list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_SOURCE_DIR}/project/BuildDependenci
 list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${CMAKE_SOURCE_DIR}/project/BuildDependencies)
 
 if(${ARCH} STREQUAL win32)
-  set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/include/python)
+  set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/Win32/include/python)
 else()
   set(PYTHON_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/project/BuildDependencies/${ARCH}/include/python)
 endif()
